e44733d32105ddce6d192e324227e8b329f33b7f
Cecilie Lynggaard Jeppesen <au661804@uni.au.dk>
2022-04-07 20:10:25 +0200

    To test tilføjet til buzzer

--- Microwave.Test.Unit/UserInterfaceTest.cs
+++ Microwave.Test.Unit/UserInterfaceTest.cs
@@ -337,6 +337,20 @@ namespace Microwave.Test.Unit
             light.Received(1).TurnOff();
         }
 
+        [Test]
+        public void Cooking_CookingIsDone_StartBuzzer()
+        {
+            powerButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetPower
+            timeButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in SetTime
+            startCancelButton.Pressed += Raise.EventWith(this, EventArgs.Empty);
+            // Now in cooking
+
+            // Cooking is done
+            uut.CookingIsDone();
+            buzzer.Received(1).StartAlarmBuz();
+        }
 
     }
 
