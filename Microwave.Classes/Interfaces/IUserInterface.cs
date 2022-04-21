fccc5b0613170edca8ab4d5ccabc364e068e9d87
Torbjørg <torbjorg-hentze@hotmail.com>
2022-04-21 13:18:23 +0200

    slettet integrationstest

--- Microwave.Classes/Interfaces/IUserInterface.cs
+++ Microwave.Classes/Interfaces/IUserInterface.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Microwave.Classes.Interfaces
 {
-    public interface IUserInterface //hey
+    public interface IUserInterface 
     {
         void OnPowerPressed(object sender, EventArgs e);
         void OnTimePressed(object sender, EventArgs e);
